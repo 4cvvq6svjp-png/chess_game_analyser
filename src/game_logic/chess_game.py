0132f331c3471a74aa07d468f060bbe4ec6274c3
Lyonnais Maximilien <maximilien.lyonnais@student-cs.fr>
2025-11-30 18:40:09 +0100

    New strategy --> do the game_logic first and then complete the specificity of the pieces and rules

--- src/game_logic/chess_game.py
+++ src/game_logic/chess_game.py
@@ -10,24 +10,31 @@ from pieces import Piece
 class chess_game():
     def __init__(self):
         self.board = Board()
-        self.turn = [0,0]
+        self.turn = 0
         self.game_is_live = True
         self.players = [Player("w"), Player("b")]
 
 
     def _is_white_turn(self) :
-        return self.turn[0] == self.turn[1]
+        return self.turn[0] == self.turn[1] 
 
 
     def coordinate(move) :
         start, end = move.strip().split('/')
         # we assume that both are of length 2, and return the coordinates as tuples
-        return (ord("a") - ord(start[0]), start[1]) , (ord("a") - ord(end[0]), end[1])
+        return (start[1], ord("a") - ord(start[0])) , (ord("a") - ord(end[0]), end[1])
 
     # to tell if a move is possible before checking the trajectory, we check if the starting square has a piece or not
-    # then if it is of the right color
-    def _could_be_a_move(self, start_square):
-        row, col = start_square # When to use coordinate function ? 
+    # then if it is of the right color and if the rows ans cols ranges are right.
+    def _could_be_a_move(self, start_square, end_square):
+        row, col = start_square # When to use coordinate function ?
+        row1, col1 = end_square
+
+        # check the range
+        if not (row in range(8) and col in range(8) and row1 in range(8) and col1 in range(8)):
+            return False
+
+        # check if the Piece exist at the staring square and if it ahs the right color
         if self.board[row][col] is not None:
             if (self._is_white_turn() and self.board[row][col].color  == "w") or\
                 (not self._is_white_turn() and self.board[row][col].color  == "b"):
@@ -37,7 +44,28 @@ class chess_game():
 
     def launch_game(self) :
         while self.game_is_live :
+            curr_player = self.players[self.turn%2]
+            # ask for a move
+            move = curr_player.tell_a_move()
 
+            # try parsing
+            try: 
+                square_from, square_to = self.coordinate(move)
+            except Exception:
+                print("Not a valid input")
+                print("Please rewrite your move")
+                continue
+            
+            # check if it points to a Piece
+            if not self._could_be_a_move(square_from) :
+                print("Your starting square is empty")
+                print("PLease try anoter input")
+                continue
+            
+            
+
+
+            
     
 
 
