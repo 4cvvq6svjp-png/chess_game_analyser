6fdb8d4f3c7a0f1dcdbad202817b4a43838f074d
Lyonnais Maximilien <maximilien.lyonnais@student-cs.fr>
2025-12-03 18:14:02 +0100

    [ADD] - bishop move was added

--- src/game_logic/bishop.py
+++ src/game_logic/bishop.py
@@ -1,14 +1,19 @@
 from pieces import Piece
+from move_utility import MoveUtility
+
 
 class Bishop (Piece):
     def __init__(self, color) :
         super().__init__(color, "bishop")
 
-    def _is_valid_move():
-        pass
+    def _is_valid_move(self, square_from: tuple, square_to: tuple, BOARD: list[list['Piece']]):
+        return MoveUtility._is_diag_valid(square_from, square_to, BOARD)
 
-    def _execute_move(self):
-        pass
 
-    def _move_piece(self, board, square, add_or_remove):
-        pass
+    def _move_piece(self, BOARD, square, add_or_remove):
+        row, col = square
+        # Change the board
+        if add_or_remove == "add" :
+            BOARD[row][col] = Bishop(self.color)
+        else :
+            BOARD[row][col] = None
--- src/game_logic/move_utility.py
+++ src/game_logic/move_utility.py
@@ -3,10 +3,31 @@ from math import copysign
 class MoveUtility :
 
     def _is_diag_valid (square_from, square_to, BOARD):
-        pass
+        # Not a diag move
+        if abs(square_from[0] - square_to[0]) != abs(square_from[1] - square_to[1]) : # XOR
+            return False
+        
+        isvalid = True
+        row, col = square_from
+        rowTO, colTO = square_to
+
+        # landing square has a same color piece on it
+        if (BOARD[rowTO][colTO] is not None) and (BOARD[row][col].color == BOARD[rowTO][colTO].color) :
+            return False
+
+        # we check if the inbetween path is free or not
+        while isvalid and (abs(row-rowTO) > 1 or abs(col-colTO) > 1) :
+            row += int(copysign(1, rowTO - row)) if rowTO!=row else 0
+            col += int(copysign(1, colTO - col)) if colTO!=col else 0
+            if BOARD[row][col] is not None :
+                isvalid = False
+        return isvalid
+
+
+
 
     def _is_line_valid (square_from, square_to, BOARD):
-        # Not a move
+        # Not a line move
         if not ((square_from[0] == square_to[0]) != (square_from[1] == square_to[1])) : # XOR
             return False
         
