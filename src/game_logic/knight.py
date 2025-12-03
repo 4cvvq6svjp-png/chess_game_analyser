85cc0bb6710cfc83f67aa545251016c900432655
Lyonnais Maximilien <maximilien.lyonnais@student-cs.fr>
2025-12-03 22:50:01 +0100

    [ADD] - added the knight movement

--- src/game_logic/knight.py
+++ src/game_logic/knight.py
@@ -8,11 +8,25 @@ class Knight (Piece) :
 
 
     
-    def _is_valid_move():
-        pass
+    def _is_valid_move(self, square_from: tuple, square_to: tuple, BOARD: list[list['Piece']]):
+        row, col = square_from
+        rowTO, colTO = square_to
 
-    def _execute_move(self):
-        pass
+        if not ((abs(row - rowTO) == 2 and abs(col - colTO)) or (abs(row - rowTO) == 1 and abs(col - colTO) == 2)) :
+            return False
 
-    def _move_piece(self, board, square, add_or_remove):
-        pass
+        # landing square has a same color piece on it
+        if (BOARD[rowTO][colTO] is not None) and (BOARD[row][col].color == BOARD[rowTO][colTO].color) :
+            return False
+        
+        return True
+
+
+
+    def _move_piece(self, BOARD, square, add_or_remove):
+        row, col = square
+        # Change the board
+        if add_or_remove == "add" :
+            BOARD[row][col] = Knight(self.color)
+        else :
+            BOARD[row][col] = None
