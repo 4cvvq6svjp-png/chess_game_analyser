2ee954ff062510e49f3c2d78717118d826124004
Lyonnais Maximilien <maximilien.lyonnais@student-cs.fr>
2025-12-03 18:17:21 +0100

    [ADD] - queen movement added

--- src/game_logic/queen.py
+++ src/game_logic/queen.py
@@ -1,5 +1,5 @@
 from pieces import Piece
-
+from move_utility import MoveUtility
 
 
 class Queen(Piece) :
@@ -8,12 +8,14 @@ class Queen(Piece) :
 
 
 
-    def _is_valid_move():
-        pass
+    def _is_valid_move(self, square_from: tuple, square_to: tuple, BOARD: list[list['Piece']]):
+        return MoveUtility._is_diag_valid(square_from, square_to, BOARD) or MoveUtility._is_line_valid(square_from, square_to, BOARD)
 
-    def _execute_move(self):
-        pass
 
-    def _move_piece(self, board, square, add_or_remove):
-        pass
-
+    def _move_piece(self, BOARD, square, add_or_remove):
+        row, col = square
+        # Change the board
+        if add_or_remove == "add" :
+            BOARD[row][col] = Queen(self.color)
+        else :
+            BOARD[row][col] = None
