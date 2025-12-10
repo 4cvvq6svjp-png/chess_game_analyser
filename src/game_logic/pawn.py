0e792a0dc1b9ba741b295adc10b46db4826ce95f
Lyonnais Maximilien <maximilien.lyonnais@student-cs.fr>
2025-12-10 19:09:45 +0100

    en passant méthod

--- src/game_logic/pawn.py
+++ src/game_logic/pawn.py
@@ -7,7 +7,6 @@ if TYPE_CHECKING:
 
 
 class Pawn (Piece) :
-    #usefull constant ??
     DIRECTION = {"w": -1, "b": 1}
 
     def __init__(self, color) :
@@ -26,7 +25,15 @@ class Pawn (Piece) :
             landing_spot = BOARD[square_to[0]][square_to[1]]
             if landing_spot is not None and landing_spot.color != self.color:
                 return True
-        
+            # is it en passant ?
+            if board.play_stack:
+                last_move = board.play_stack[-1]
+                print(last_move, abs(last_move["square_from"][0] - last_move["square_to"][0]), abs(last_move["square_to"][1] - square_to[1]))
+                if last_move["piece"] == "pawn"\
+                    and abs(last_move["square_from"][0] - last_move["square_to"][0]) == 2\
+                    and last_move["square_to"][1] == square_to[1]:
+                    return True
+
         # then it is a move forward
         if square_from[1] == square_to[1] :
             if square_to[0] - square_from[0] == m:
@@ -34,8 +41,7 @@ class Pawn (Piece) :
             elif square_to[0] - square_from[0] == 2*m and square_from[0] == Pawn.starting_rank(self.color):
                 return True
             
-        # Is it en passant ? 
-        
+
         return False
         
 
